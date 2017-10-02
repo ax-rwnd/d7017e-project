90fdfdc7683ae754eac84c969155f357dcb9b7f0
bosse93 <boose_93@hotmail.com>
2017-10-02 14:31:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(2223, function () {
    var host = server.address().address
    var port = server.address().port
    
