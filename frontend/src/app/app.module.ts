8e71f40544ed4db7bed70657b381363b5c080cd4
axel <axel@familjenwallin.se>
2017-10-02 16:56:09 +0200

    Added components and routing
    Login, statistics and routing added. Changed app.component.html to only include the routing and set the login page as default.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import {RouterModule, Routes} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'statistics', component: StatisticsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatisticsComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
