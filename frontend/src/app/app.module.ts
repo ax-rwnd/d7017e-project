bd05b72178014c5722105719ca6cbbb09e15cade
axel <axel@familjenwallin.se>
2017-10-03 17:23:47 +0200

    Added the codemirror directive to the assignment component.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-//NG-BOOTSTRAP
+// NG-BOOTSTRAP
 import { AlertModule } from 'ngx-bootstrap';
 import { ButtonsModule } from 'ngx-bootstrap';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+import { CodemirrorModule } from 'ng2-codemirror';
 
 import { AppComponent } from './app.component';
 import { StatisticsComponent } from './statistics/statistics.component';
@@ -13,14 +16,16 @@ import {RouterModule, Routes} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import {HeadComponent} from './head/head.component';
 import {UserComponent} from './user/user.component';
+import { AssignmentComponent } from './assignment/assignment.component';
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'statistics', component: StatisticsComponent },
   { path: 'head', component: HeadComponent },
+  { path: 'assignment', component: AssignmentComponent },
   { path: 'user', component: UserComponent }
 ];
-import { AssignmentComponent } from './assignment/assignment.component';
+
 
 @NgModule({
   declarations: [
@@ -34,6 +39,8 @@ import { AssignmentComponent } from './assignment/assignment.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    CodemirrorModule,
     AlertModule.forRoot(),
     ButtonsModule.forRoot(),
     BsDropdownModule.forRoot(),
--- frontend/src/app/assignment/assignment.component.ts
+++ frontend/src/app/assignment/assignment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import 'codemirror/mode/go/go';
 
 @Component({
   selector: 'app-assignment',
@@ -6,10 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment.component.css']
 })
 export class AssignmentComponent implements OnInit {
-
+  content: string;
   constructor() { }
 
   ngOnInit() {
+    this.content = 'function StringStream(string) {\n' +
+      '  this.pos = 0;\n' +
+      '  this.string = string;\n' +
+      '}\n' +
+      '\n' +
+      'StringStream.prototype = {\n' +
+      '  done: function() {return this.pos >= this.string.length;},\n' +
+      '  peek: function() {return this.string.charAt(this.pos);},\n' +
+      '  next: function() {\n' +
+      '    if (this.pos < this.string.length)\n' +
+      '      return this.string.charAt(this.pos++);\n' +
+      '  },\n' +
+      '  eat: function(match) {\n' +
+      '    var ch = this.string.charAt(this.pos);\n' +
+      '    if (typeof match == "string") var ok = ch == match;\n' +
+      '    else var ok = ch && match.test ? match.test(ch) : match(ch);\n' +
+      '    if (ok) {this.pos++; return ch;}\n' +
+      '  },\n' +
+      '  eatWhile: function(match) {\n' +
+      '    var start = this.pos;\n' +
+      '    while (this.eat(match));\n' +
+      '    if (this.pos > start) return this.string.slice(start, this.pos);\n' +
+      '  },\n' +
+      '  backUp: function(n) {this.pos -= n;},\n' +
+      '  column: function() {return this.pos;},\n' +
+      '  eatSpace: function() {\n' +
+      '    var start = this.pos;\n' +
+      '    while (/\\s/.test(this.string.charAt(this.pos))) this.pos++;\n' +
+      '    return this.pos - start;\n' +
+      '  },\n' +
+      '  match: function(pattern, consume, caseInsensitive) {\n' +
+      '    if (typeof pattern == "string") {\n' +
+      '      function cased(str) {return caseInsensitive ? str.toLowerCase() : str;}\n' +
+      '      if (cased(this.string).indexOf(cased(pattern), this.pos) == this.pos) {\n' +
+      '        if (consume !== false) this.pos += str.length;\n' +
+      '        return true;\n' +
+      '      }\n' +
+      '    }\n' +
+      '    else {\n' +
+      '      var match = this.string.slice(this.pos).match(pattern);\n' +
+      '      if (match && consume !== false) this.pos += match[0].length;\n' +
+      '      return match;\n' +
+      '    }\n' +
+      '  }\n' +
+      '};';
   }
 
 }
