e351d60664dea208a356390f41e6ab613e8e37fa
Axel <axel-sundbom@hotmail.com>
2017-10-04 13:12:26 +0200

    Adds router and ping.

--- tester/runner/server.js
+++ tester/runner/server.js
@@ -14,7 +14,7 @@ if (args.length < 1) {
 
 //consider using express
 
-http.createServer(function (req, res) {
+function handleTest(req, res) {
     const { headers, method, url } = req
 
     var body = []
@@ -28,16 +28,46 @@ http.createServer(function (req, res) {
         try {
             //Convert the array of Buffers to a javascript object
             body = JSON.parse(Buffer.concat(body).toString())
-            let response = test_runner.runTests(body).then( (resp) => {
-                const resBody = { headers, method, url, resp }
-                res.writeHead(200, {'Content-Type': 'application/json'})
-                res.write(JSON.stringify(resBody))
-                res.end()
-            })
+            test_runner.runTests(body)
+                .then( (resp) => {
+                    const resBody = { headers, method, url, resp }
+                    res.writeHead(200, {'Content-Type': 'application/json'})
+                    res.write(JSON.stringify(resBody))
+                    res.end()
+                })
+                .catch((e) => {
+                    console.log(e)
+                    //TODO: handle errors separately
+                    res.statusCode = 500
+                    res.end()
+                })
         } catch (e) {
             console.log(e)
             res.statusCode = 400
             res.end()
         }
     });
+}
+
+function handlePing(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end()
+}
+
+http.createServer(function (req, res) {
+    const { headers, method, url } = req
+
+    if (url === "/") {
+        try {
+            handleTest(req, res)
+        } catch (e) {
+            console.log(e)
+        }
+    } else if (url === "/ping") {
+        try {
+            handlePing(req, res)
+        } catch (e) {
+            console.log(e)
+        }
+    }
 }).listen(port)
