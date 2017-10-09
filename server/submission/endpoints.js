ceabab6a01076d810ed22cef711886afc2ea986a
mjolnir92 <xerzeon@gmail.com>
2017-10-09 17:28:40 +0200

    Add dir and files for endpoints for submissions and tests

---
+++ server/submission/endpoints.js
@@ -0,0 +1,15 @@
+/*
+API endpoints for submissions (user submitted code)
+
+Requests to these endpoints should start with "<url>/submission"
+
+Suggested endpoints:
+	GET: /submission
+		Get all submissions from database
+	POST: /submission
+		Post a submission to database and returns its id
+	GET: /submission/<course>
+		Get all submissions related to "course" from database
+	GET: /submission/<course>/<id>
+		Get submission from "course" with id "id"
+*/
---
+++ server/test/endpoints.js
@@ -0,0 +1,9 @@
+/*
+API endpoints for tests (user code sent for testing)
+
+Requests to these endpoints should start with "<url>/test"
+
+Suggested endpoints:
+	POST: /test
+		Post code to test server and return result
+*/
