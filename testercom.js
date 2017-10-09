58997e303a8dfd637f7661d00059e1db9f2d7dab
Tobias <tobias91.axelsson@gmail.com>
2017-10-09 16:05:08 +0200

    testercom

---
+++ testercom.js
@@ -0,0 +1,9 @@
+var app = require('express')();
+var mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost/test';
+
+mongoose.connect(dbURI);
+
+function getTest(assignmentID) {
+    
+} 
