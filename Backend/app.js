dda06161004c8982755c7677cb70ea8f62207efa
spikey91 <tobias91.axelsson@gmail.com>
2017-10-12 17:52:30 +0200

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,7 @@ var app = express();
 
 //Connect to db
 //If you have no mongodb running. Comment the below to prevent the app from crashing at start.
-mongoose.connect('130.240.5.132.27017');
+mongoose.connect('130.240.5.132:27017');
 
 
 app.use(bodyParser.json());
