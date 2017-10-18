b5c6ca66f6f6a9c34a1eec8bb81e86b0da9afde8
Axel <axel-sundbom@hotmail.com>
2017-10-18 18:57:30 +0200

    Refactors stat page code, visual regression.

--- tester/server.js
+++ tester/server.js
@@ -1,4 +1,5 @@
-use('strict');
+/* jshint node: true */
+'use strict';
 
 var express = require('express');
 var execFile = require('child_process').execFile;
@@ -18,77 +19,92 @@ if (args.length < 1) {
     port = parseInt(args[0]);
 }
 
+function formatQueues(langs, queues) {
+	// Format the queues
+
+	var s = '';
+	langs.forEach(function(lang){
+		s += 	`<tr>
+					<td>${lang}</td>
+					<td>${manager.queue[lang].length}</td>
+				</tr>`;
+	});
+
+	return s;
+}
+
+function formatLanguages(langs, containers) {
+    // Format the containers for each language into strings
+
+    var formatted = '';
+    langs.forEach((lang) => {
+        containers[lang].forEach((container) => {
+            formatted += formatContainer(lang, container);
+        });
+    });
+
+    return formatted;
+}
+
+function formatContainer(lang, container) {
+    // Format the status of a container into a string
+
+    var time_alive = (Date.now()-container.time_created);
+    var t = new Date(null);
+	t.setSeconds(time_alive/1000); // specify value for SECONDS here
+	var aliveString = t.toISOString().substr(11, 8);
+
+    const s = `
+        <tr>
+            <td>${lang}</td>
+            <td>${container.id}</td>
+            <td>${container.port}</td>
+            <td>${container.used}</td>
+            <td>${container.ready}</td>
+            <td>${new Date(container.time_created).toString()}</td>
+            <td>${aliveString}</td>
+        </tr>
+    `;
+
+    return s;
+}
+
 // App
 const app = express();
 app.get('/', (req, res) => {
 
-    var now = Date.now();
-    var s = '';
+    const s = `
+        <h1>Tester</h1>
+            <div style="float: left; width: 70%;">
+            <h2>Containers</h2>
+            <table style="border-spacing: 20px 0;">
+        <tr>
+            <th>Language</th>
+            <th>ID</th>
+            <th>Port</th>
+            <th>Used</th>
+            <th>Ready</th>
+            <th>Time created</th>
+            <th>Time alive</th>
+        </tr>
 
-    s += '<h1>Tester</h1>';
+        ${formatLanguages(manager.LANGS, manager.containers)}
 
-    s += '<div style="float: left; width: 70%;">';
-    s += '<h2>Containers</h2>';
-    s += '<table style="border-spacing: 20px 0;">';
-    s += '<tr><th>Language</th><th>ID</th><th>Port</th><th>Used</th>'+
-    '<th>Ready</th><th>Time created</th><th>Time alive</th></tr>';
-    manager.LANGS.forEach(function(lang) {
-    for(var i = 0; i < manager.containers[lang].length; i++) {
-        var container = manager.containers[lang][i];
+        </table>
+        </div>
 
-        var time_alive = (now-container.time_created)/1000;
+        <div style="float: right; width: 30%;">
+            <h2>Queue</h2>
+            <table>
+            <tr>
+                <th>Language</th>
+                <th>#</th>
+            </tr>
 
-        var days    = Math.floor(time_alive/(60*60*24));
-        time_alive  = time_alive%(60*60*24);
-        var hours   = Math.floor(time_alive/(60*60));
-        time_alive  = time_alive%(60*60);
-        var minutes = Math.floor(time_alive/(60));
-        time_alive  = time_alive%(60);
-        var seconds = Math.floor(time_alive);
-
-        time_alive = "";
-        if(days > 0) {
-            time_alive += days+"d";
-        }
-        if(hours > 0) {
-            time_alive += hours+"h";
-        }
-        if(minutes > 0) {
-            time_alive += minutes+"m";
-        }
-        time_alive += seconds+"s";
-
-
-        if(time_alive < 60) {
-            time_alive = Math.floor(time_alive) + "s";
-        } else if(time_alive > 60) {
-            time_alive = Math.floor(time_alive/60) + "m";
-        } else if(time_alive > 60*60) {
-            time_alive = Math.floor(time_alive/(60*60)) + "h";
-        } else if(time_alive > 60*60*24) {
-            time_alive = Math.floor(time_alive/(60*60*24)) + "d";
-        }
-
-        s += '<tr><td>'+lang+'</td><td>'+container.id+'</td>'+
-            '<td>'+container.port+'</td><td>'+container.used+'</td>'+
-            '<td>'+container.ready+'</td>'+
-            '<td>'+new Date(container.time_created).toString()+'</td>'+
-            '<td>'+time_alive+'</td></tr>';
-        }
-    });
-
-    s += '</table>';
-    s += '</div>';
-
-    s += '<div style="float: right; width: 30%;">';
-    s += '<h2>Queue</h2>';
-    s += '<table>';
-    s += '<tr><th>Language</th><th>#</th></tr>';
-    manager.LANGS.forEach(function(lang) {
-        s += '<tr><td>'+lang+'</td><td>'+manager.queue[lang].length+'</td></tr>';
-    });
-    s += '</table>';
-    s += '</div>';
+			${formatQueues(manager.LANGS, manager.queue)}
+            </table>
+        </div>
+    `;
 
     res.send(s);
 });
@@ -98,4 +114,4 @@ app.post('/', (req, res) => {
 });
 
 app.listen(port, HOST);
-console.log('Running on http://${HOST}:${port}');
+console.log(`Running on http://${HOST}:${port}`);
