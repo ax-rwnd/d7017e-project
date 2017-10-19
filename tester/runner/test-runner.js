79028f5cb66262ac979401632a17db2189fe31d5
nilfit <nils.fitinghoff@gmail.com>
2017-10-19 11:58:05 +0200

    add linter test type

--- tester/runner/test-runner.js
+++ tester/runner/test-runner.js
@@ -19,28 +19,39 @@ async function runTests(request) {
     const codeFile = tmp.fileSync();
     fs.writeFileSync(codeFile.fd, request.code);
 
-    let res = {results: []};
+    let res = {results: {io: []}};
     let langModule = resolveLanguage(request.lang);
     let executable = langModule.prepare(codeFile.name);
 
-    for (const test of request.tests) {
+    // Run mandatory IO tests
+    for (const test of request.tests.io) {
         try {
             let output = await validateAndRun(executable, test, langModule);
             if (output.stdout !== test.stdout) {
                 // TODO: tell them what broke our expectations
-                res.results.push(new result(test.id, false, output.stderr));
+                res.results.io.push(new result(test.id, false, output.stderr));
             } else {
-                res.results.push(new result(test.id, true, output.stderr));
+                // test succeeded
+                res.results.io.push(new result(test.id, true, output.stderr));
             }
         } catch (e) {
             // SIGTERM is sent to the child process on timeout
             if (e.signal == 'SIGTERM') {
                 // TODO: tell them that the failure was from a timeout
                 console.log('Execution timed out on test:', test.id);
-                res.results.push(new result(test.id, false, ''));
+                res.results.io.push(new result(test.id, false, ''));
             } else {
                 throw e;
             }
+        }
+    }
+
+    // Lint
+    if (request.lint === true) {
+        try {
+            res.results.lint = await request.tests.lint(codeFile);
+        } catch (e) {
+            res.results.lint = 'Unknown linter error';
         }
     }
 
@@ -69,6 +80,9 @@ function resolveLanguage(lang) {
     }
     if (!langModule.hasOwnProperty('prepare')) {
         langModule.prepare = (file) => {return file;};
+    }
+    if (!langModule.hasOwnProperty('lint')) {
+        langModule.prepare = (file) => {return '';};
     }
     return langModule;
 }
