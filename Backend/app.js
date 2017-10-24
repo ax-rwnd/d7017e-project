0169b5c96a590dc15e093f182ccc09b2e3b971bb
Tobias <tobias91.axelsson@gmail.com>
2017-10-24 15:50:56 +0200

    fixed merge confl. organized/cleaned up code. ver2

--- Backend/app.js
+++ Backend/app.js
@@ -34,20 +34,20 @@ app.use('/api', api);
 
 //Route not found.
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 //Error in server. Basically http error 500, internal server error.
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  //res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    //res.locals.message = err.message;
+    //res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send("HTTP error: " + err.status + ". " + err.message);
+    // render the error page
+    res.status(err.status || 500);
+    res.send("HTTP error: " + err.status + ". " + err.message);
 });
 
 
