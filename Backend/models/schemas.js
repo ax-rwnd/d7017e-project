89836a023d8cd3488c813c9808a05b99fa4fd6cd
Mikael Hedkvist <mikhed-1@student.ltu.se>
2017-10-26 15:43:12 +0200

    Add User schema

--- Backend/models/schemas.js
+++ Backend/models/schemas.js
@@ -14,9 +14,17 @@ var assignmentSchema = new Schema({
     tests: [{ type: Schema.Types.ObjectId, ref: 'Test', required: true }]
 });
 
+var userSchema = new Schema({
+	username: {type: String, required: true},
+    email: {type: String, required: true},
+    admin: {type: boolean, required: true}
+    courses: [{ type: Schema.Types.ObjectId, ref: 'Course', required: true }]
+});
+
 ////
 var Test = mongoose.model('Test', testSchema);
 var Assignment = mongoose.model('Assignment', assignmentSchema);
-var Models = {Assignment: Assignment, Test: Test};
+var User = mongoose.model('User', userSchema);
+var Models = {Assignment: Assignment, Test: Test, User: User};
 
 module.exports = Models;
