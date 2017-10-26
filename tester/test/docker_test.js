5c32cc1419d7789902a51a0da57e87935fc24cf5
Riwzi <rickardnordlander@hotmail.com>
2017-10-26 19:14:56 +0200

    add java and c unit tests

--- tester/test/docker_test.js
+++ tester/test/docker_test.js
@@ -108,4 +108,100 @@ describe('language tests', function() {
             });
         });
     }).timeout(10000);
+
+    it('java test', function(done) {
+        const req = {
+            'lang':'java',
+            'code':'public class main {\npublic static void main(String[] args){\nfor (String s: args) {\nSystem.out.println(s);\n}\n}\n}',
+            'tests': {
+                'io': [
+                    {'stdin': '', 'args': [5], 'stdout': '5\n', 'id': 0},
+                    {'stdin': 'hi', 'args': [], 'stdout' :'bad test\n', 'id': 1}
+                ],
+                'lint': false
+            },
+            'optional_tests': []
+        };
+
+        const expected = {
+            results: {
+                io: [
+                    {id: 0, ok: true, stderr: ''},
+                    {id: 1, ok: false, stderr: ''}
+                ],
+                prepare: '',
+                code_size: 111
+            }
+        };
+
+        startRunner(req.lang).then(container => {
+            request.post({
+                url: 'http://' + container.extra.address + '/',
+                headers: {'Content-Type': 'application/json'},
+                json: req
+            }, (error, response, body) => {
+                if(error) {
+                    throw error;
+                }
+
+                body.resp.results.io.forEach(iores => {
+                    // just test that the type is correct since the time varies
+                    assert.equal(typeof(iores.time), 'number');
+                    // delete it so it's not checked later
+                    delete iores.time;
+                });
+                assert.equal(JSON.stringify(expected.results), JSON.stringify(body.resp.results));
+                container.stop();
+                done();
+            });
+        });
+    }).timeout(10000);
+
+    it('c test', function(done) {
+        const req = {
+            'lang':'c',
+            'code':'#include<stdio.h>\nint main(){\nprintf("Hello World");\nfflush(stdout);\nreturn 0;\n}',
+            'tests': {
+                'io': [
+                    {'stdin': '', 'args': [], 'stdout': 'Hello World', 'id': 0},
+                    {'stdin': 'hi', 'args': [], 'stdout' :'bad test\n', 'id': 1}
+                ],
+                'lint': false
+            },
+            'optional_tests': []
+        };
+
+        const expected = {
+            results: {
+                io: [
+                    {id: 0, ok: true, stderr: ''},
+                    {id: 1, ok: false, stderr: ''}
+                ],
+                prepare: '',
+                code_size: 80
+            }
+        };
+
+        startRunner(req.lang).then(container => {
+            request.post({
+                url: 'http://' + container.extra.address + '/',
+                headers: {'Content-Type': 'application/json'},
+                json: req
+            }, (error, response, body) => {
+                if(error) {
+                    throw error;
+                }
+
+                body.resp.results.io.forEach(iores => {
+                    // just test that the type is correct since the time varies
+                    assert.equal(typeof(iores.time), 'number');
+                    // delete it so it's not checked later
+                    delete iores.time;
+                });
+                assert.equal(JSON.stringify(expected.results), JSON.stringify(body.resp.results));
+                container.stop();
+                done();
+            });
+        });
+    }).timeout(10000);
 });
