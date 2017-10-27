b3a061806ca9f4b612106c121e09d098c7eb5a70
axel <axel@familjenwallin.se>
2017-10-27 15:42:40 +0200

    Added some tests in course service. Changed the way the form works.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // NG-BOOTSTRAP
 import { AlertModule } from 'ngx-bootstrap';
@@ -55,6 +55,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     CodemirrorModule,
     AlertModule.forRoot(),
