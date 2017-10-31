8bb9c22a9a63f46e22543e719e2a12c69fe40117
andersandrew <bombanders@gmail.com>
2017-10-31 15:44:33 +0100

    test uupdated

--- frontend/src/app/head/head.component.spec.ts
+++ frontend/src/app/head/head.component.spec.ts
@@ -1,18 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from '../app.component';
 import { HeadService } from '../services/head.service';
-import { CourseService } from '../services/course.service';
-
 import { HeadComponent } from './head.component';
+import { CourseService } from '../services/course.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeadComponent', () => {
   let component: HeadComponent;
   let fixture: ComponentFixture<HeadComponent>;
+  let service: HeadService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ HeadComponent ],
-      providers: [ AppComponent, HeadService, CourseService ]
+      providers: [ AppComponent, HeadService, CourseService ],
+      imports: [RouterTestingModule],
     })
     .compileComponents();
   }));
@@ -21,5 +23,11 @@ describe('HeadComponent', () => {
     fixture = TestBed.createComponent(HeadComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should check toggle function', () => {
+    const tg = component.sidebarState;
+    component.toggleState();
+    expect(tg).toBe('active');
   });
 });
