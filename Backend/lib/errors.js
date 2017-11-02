5c8a4a6e3b00a41d85cf66b0fb215e5bab579b1e
bosse93 <frebos-3@student.ltu.se>
2017-11-02 16:32:04 +0100

    custom errors

--- Backend/lib/errors.js
+++ Backend/lib/errors.js
@@ -1,11 +1,21 @@
 'use strict';
-const errors = require('throw.js');
+var util = require('util');
 // This file defines enums for easy error handling
 
+function APIError(message, httpCode, errorCode,) {
+            this.name = this.constructor.name;
+            this.message = message;
+            this. httpCode = httpCode;
+            this.errorCode = errorCode;
+
+            //include stack trace in error object
+}
+util.inherits(APIError, Error);
+
+
 module.exports = {
-    TEST: {code: 666, msg:"This is a test error"},
-    TOKEN_USER_NOT_FOUND: new errors.CustomError("User not found", "Internal Server Error", 500, 7000),
-    INVALID_TOKEN: new errors.CustomError("Invalid Token Error", "Invalid Token", 401, 7001),
-    NO_COURSES_EXIST: new errors.CustomError("Course Error", "No courses exists", 404, 7002),
-    COURSE_DO_NOT_EXIST: new errors.CustomError("Course Error", "Course doesn't exist", 404, 7003)
+    TOKEN_USER_NOT_FOUND: new APIError("Internal Server Error", 500, 7000),
+    INVALID_TOKEN: new APIError("Invalid Token", 401, 7001),
+    NO_COURSES_EXIST: new APIError("No courses exists", 404, 7002),
+    COURSE_DO_NOT_EXIST: new APIError("Course doesn't exist", 404, 7003)
 };
