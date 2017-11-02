fb8ae691da5dd341de5ca52d55c8dfb510a56408
Riwzi <rickardnordlander@hotmail.com>
2017-11-02 16:42:43 +0100

    fix issue appearing when the appScroller directive is added on an element that is not scrollable

--- frontend/src/app/scroller.directive.ts
+++ frontend/src/app/scroller.directive.ts
@@ -10,17 +10,21 @@ export class ScrollerDirective {
   constructor(private elRef: ElementRef, private renderer: Renderer) {
     renderer.listen(elRef.nativeElement, 'wheel', (e) => {
         const element = elRef.nativeElement;
-        if (element.scrollTop + element.offsetHeight > element.scrollHeight) {
-          if (e.deltaY > 0) {
-            e = e || window.event;
-          if (e.preventDefault) {
-            e.preventDefault();
-          }
-          e.returnValue = false;
-          }
-        } else if (element.scrollTop === 0 && e.deltaY < 0) {
-          if (e.preventDefault) {
-            e.preventDefault();
+        const isScrollable = element.scrollWidth > element.clientWidth || element.scrollHeight > element.clientHeight;
+
+        if (isScrollable) {
+          if (element.scrollTop + element.offsetHeight > element.scrollHeight) {
+            if (e.deltaY > 0) {
+              e = e || window.event;
+            if (e.preventDefault) {
+              e.preventDefault();
+            }
+            e.returnValue = false;
+            }
+          } else if (element.scrollTop === 0 && e.deltaY < 0) {
+            if (e.preventDefault) {
+              e.preventDefault();
+            }
           }
         }
     });
