7b80b6808bb7fe97113f47d1f17efe0e184a80c8
Tobias <tobias91.axelsson@gmail.com>
2017-11-02 11:52:22 +0100

    added and tested helper funtion for features: passAllMandatoryTests

--- Backend/features/features_helper.js
+++ Backend/features/features_helper.js
@@ -1,8 +1,15 @@
 'use strict';
 
+//see if all mandatory tests are passed
 function passAllMandatoryTests(data) {
 
-    
+    mandatoryTests = data.results.io;
+
+    for (i = 0; i < mandatoryTests.length; i++) {
+        if mandatoryTests[i].ok == false {
+            return false;
+        }
+    }
 
     return true;
 }
