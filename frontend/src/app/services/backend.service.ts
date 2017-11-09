247682f858c3de3e6073e3310089221f90a63e85
axel <axel@familjenwallin.se>
2017-11-09 17:24:59 +0100

    Added get for assignments, you can now access all available assignments and not only the default one.

--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -91,6 +91,9 @@ export class BackendService {
 
     // stub
   }
+  getCourseAssignments(course_id: string) {
+    return this.apiGet('/api/courses/' + course_id + '?fields=assignments');
+  }
 
   postNewBadge(icon: string, title: string, description: string) {
     const body = {'icon': icon, 'title': title, 'description': description};
