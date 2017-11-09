bca84dcae2f259ad48e32fe7fd0e72e6246ca7b4
carnufex <carnufex@users.noreply.github.com>
2017-11-09 14:50:02 +0100

    retryRequest skeleton

--- frontend/src/app/Interceptors/Auth.interceptor.ts
+++ frontend/src/app/Interceptors/Auth.interceptor.ts
@@ -10,62 +10,53 @@ import 'rxjs/add/operator/do';
 import { environment } from '../../environments/environment';
 import {Http, RequestOptions, Headers} from '@angular/http';
 
+
 @Injectable()
 export class NoopInterceptor implements HttpInterceptor {
   access_token: string;
 
-  constructor(public auth: AuthService, private router: Router, private http: Http) {}
-
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('interceptor running');
-    const authHeader = this.auth.getToken();
-    console.log('this is authHeader: ' + authHeader);
-    // kräver en fking localstorage för att fungera.......
-    const authReq = req.clone({headers: req.headers.set('Authorization', authHeader)});
-    return next.handle(authReq);
-  }
-
+  constructor(private auth: AuthService, private router: Router, private http: Http) {}
 
   /*
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      console.log('interceptor running');
       const authHeader = this.auth.getToken();
-      console.log('interceptet token: ' + authHeader);
-      const authReq = request.clone({headers: request.headers.set('Authorization', authHeader)});
-      return next.handle(authReq).do((event: HttpEvent<any>) => {
-        if (event instanceof HttpResponse) {
-          // do stuff with response if you want
-        }
-      }, (err: any) => {
-        if (err instanceof HttpErrorResponse) {
-          if (err.status === 401) {
-            // set refresh token header
+      console.log('this is authHeader: ' + authHeader);
+      // kräver en fking localstorage för att fungera.......
+      const authReq = req.clone({headers: req.headers.set('Authorization', authHeader)});
+      return next.handle(authReq);
+    }
+  */
 
-            const headers = new Headers();
-            headers.append('Authorization', this.auth.getRefreshToken());
-            console.log(this.auth.getRefreshToken());
-            const options = new RequestOptions({ headers: headers });
 
-            this.http.get(environment.backend_ip + '/auth/accesstoken', options).subscribe(
-              data => {
-                console.log('THIS IS DATA');
-                console.log(data);
-                this.access_token = data['access_token'];
-                console.log('access_token: ' + this.access_token);
-                localStorage.setItem('token', this.access_token);
-
-              },
-              err => {
-                console.log(err);
-
-              }
-
-            );
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authHeader = this.auth.getToken();
+    // console.log('interceptet token: ' + authHeader);
+    const authReq = request.clone({headers: request.headers.set('Authorization', authHeader)});
+    return next.handle(authReq).do((event: HttpEvent<any>) => {
+      if (event instanceof HttpResponse) {
+        // do stuff with response if you want
+      }
+    }, (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          // caching 401 request
+          this.auth.collectFailedRequest(authReq);
+          // requesting new token from backend
+          this.auth.requestNewToken().subscribe( resp => {
+            if (!resp) {
+              console.log('no response');
+            } else {
+              // retry failed requests
+              // this.auth.retryFailedRequests();
+              // return next.handle(authReq);
+            }
+          });
 
         }
       }
     });
   }
-  */
+
 
 }
