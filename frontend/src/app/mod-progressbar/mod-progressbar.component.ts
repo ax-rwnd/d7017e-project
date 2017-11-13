73334f4500fc1eaca2029f0cb34df074530539e4
andersandrew <bombanders@gmail.com>
2017-11-13 13:06:07 +0100

    back

--- frontend/src/app/mod-progressbar/mod-progressbar.component.ts
+++ frontend/src/app/mod-progressbar/mod-progressbar.component.ts
@@ -3,27 +3,10 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-mod-progressbar',
   templateUrl: './mod-progressbar.component.html',
-  styleUrls: ['../gameelement/gameelement.component.css']
 })
 export class ModProgressbarComponent implements OnInit {
   // Defines a progress bar, suitable for use in courses
   @Input() progress: number;
-  assignmentz: {};
 
   ngOnInit() {
-  	test();
-  }
-
-}
-
-function test(){
-  	for (let i=0; i<4; i++){
-  		assignmentz{i} = {id: i, name: 'name s', available: true}
-  	}
-		console.log('see amount', this.assignmentz);
-}
-interface assignmentz {
-	id: number;
-	name: string;
-	available: boolean;
-}
+ }
