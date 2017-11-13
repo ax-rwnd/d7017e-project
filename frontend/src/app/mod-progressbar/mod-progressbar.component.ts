b5338575d25ded2f20c642044da474ec5710b619
andersandrew <bombanders@gmail.com>
2017-11-13 13:00:18 +0100

    moved adventuremap to own file

--- frontend/src/app/mod-progressbar/mod-progressbar.component.ts
+++ frontend/src/app/mod-progressbar/mod-progressbar.component.ts
@@ -8,7 +8,22 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ModProgressbarComponent implements OnInit {
   // Defines a progress bar, suitable for use in courses
   @Input() progress: number;
+  assignmentz: {};
 
   ngOnInit() {
+  	test();
   }
+
+}
+
+function test(){
+  	for (let i=0; i<4; i++){
+  		assignmentz{i} = {id: i, name: 'name s', available: true}
+  	}
+		console.log('see amount', this.assignmentz);
+}
+interface assignmentz {
+	id: number;
+	name: string;
+	available: boolean;
 }
