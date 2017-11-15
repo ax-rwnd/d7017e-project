e48e686f66541ba5a4ce642be08009cb8334cb23
Axel <axel-sundbom@hotmail.com>
2017-11-15 13:07:12 +0000

    Reverts progressbar to initial state.

--- frontend/src/app/mod-progressbar/mod-progressbar.component.ts
+++ frontend/src/app/mod-progressbar/mod-progressbar.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mod-progressbar',
-  templateUrl: './mod-progressbar.component.html',	
+  templateUrl: './mod-progressbar.component.html',
+  styleUrls: ['../gameelement/gameelement.component.css']
 })
 export class ModProgressbarComponent implements OnInit {
   // Defines a progress bar, suitable for use in courses
   @Input() progress: number;
 
   ngOnInit() {
- }}
+  }
+}
