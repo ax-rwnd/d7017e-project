26ab415443fd7f5f4c9ea1304a7d3a210fed4ad4
Riwzi <rickardnordlander@hotmail.com>
2017-11-16 15:50:32 +0100

    add endpoint on backendService to save a code draft

--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -120,6 +120,13 @@ export class BackendService {
     return this.apiPost('/api/courses/' + course_id.get() + '/assignments/' + assignment_id.get() + '/submit', body);
   }
 
+  postDraft(course_id: ObjectID, assignment_id: ObjectID, code: string, lang: string) {
+    // Saves a draft of the editors content on backend
+
+    const body = {'code': code, 'lang': lang};
+    return this.apiPost('/api/courses/' + course_id.get() + '/assignments/' + assignment_id.get() + '/save', body);
+  }
+
   createAssignment(description: string, tests: any) {
     // Creates an assignment with some tests
 
