774be8368a0dcd6989dacb9c889dd638b9d5a8e4
axel <axel@familjenwallin.se>
2017-11-16 13:17:24 +0100

    Leaderboard component now updates when input (course id) changes.

--- frontend/src/app/mod-leaderboard/mod-leaderboard.component.ts
+++ frontend/src/app/mod-leaderboard/mod-leaderboard.component.ts
@@ -1,5 +1,5 @@
 import { GameelementComponent } from '../gameelement/gameelement.component';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-mod-leaderboard',
@@ -7,11 +7,19 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['../gameelement/gameelement.component.css']
 })
 
-export class ModLeaderboardComponent extends GameelementComponent implements OnInit {
+export class ModLeaderboardComponent extends GameelementComponent implements OnChanges, OnInit {
   @Input() courseCode: string;
   public leaderList: any[];
 
   ngOnInit() {
+    this.update();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('changes: ', changes);
+    this.update();
+  }
+  update() {
     this.backendService.getCourseUsers(this.courseCode).then(data => {
       // TODO: the call will look something like this once
       //  backend implements the API endpoint, however, the coursecode needs
