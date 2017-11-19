ba4abb870150e966642370055519a7af01a93f1d
Axel <axel-sundbom@hotmail.com>
2017-11-19 17:47:34 +0000

    Stylistic fixes...

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -4,8 +4,7 @@ import { UserService } from '../services/user.service';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import { environment } from '../../environments/environment';
-import {ActivatedRoute, Router} from "@angular/router";
-
+import {ActivatedRoute, Router} from '@angular/router';
 
 class User {
   id: string;
@@ -24,6 +23,7 @@ class User {
 export class LoginComponent implements OnInit {
   user: any;
   data;
+
   constructor(private userService: UserService, public http: HttpClient) {
   }
   results: string;
@@ -31,7 +31,6 @@ export class LoginComponent implements OnInit {
   userMe: User[];
   observableUser: Observable<User[]>;
   errorMessage: String;
-
 
   ngOnInit() {
     this.user = this.userService.userInfo;
@@ -43,9 +42,3 @@ export class LoginComponent implements OnInit {
     window.location.href = redirect;
   }
 }
-
-
-
-/*
-
- */
