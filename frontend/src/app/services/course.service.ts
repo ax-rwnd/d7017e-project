c4f27764dfc20c1dc98803542eefea9a334f4a59
Riwzi <rickardnordlander@hotmail.com>
2017-11-21 16:15:50 +0100

    remove old hardcoded courses

--- frontend/src/app/services/course.service.ts
+++ frontend/src/app/services/course.service.ts
@@ -9,59 +9,6 @@ export class CourseService {
   updated = false;
   constructor(private backendService: BackendService, private assignmentService: AssignmentService) {
     this.courses = [];
-    /***hardcoded info later gotten from database***/
-    /*let lbEntry1 = {name: 'anonymous', score: 80};
-    let lbEntry2 = {name: 'anonymous', score: 76};
-    let lbEntry3 = {name: 'you', score: 70};
-    let lbEntry4 = {name: 'anonymous', score: 67};
-    let lbEntry5 = {name: 'anonymous', score: 65};
-    let leaderboard = [lbEntry1, lbEntry2, lbEntry3, lbEntry4, lbEntry5];
-    const rewards1 = newRewards(50, 70, ['silver_medal_badge', 'bronze_trophy_badge'], leaderboard);
-    const course0 = newCourse('0', 'Introduction to programming', 'D0009E', '# Information\n' +
-      '\n' +
-      '## Kursens mål\n' +
-      '*Att ge en grundlig introduktion till datorbaserad problemlösning med hjälp av ett modern imperativt programmeringsspråk*\n' +
-      '\n' +
-      '## Innehåll\n' +
-      '\n' +
-      '* Introduktion till programutveckling och programutvecklingsmiljöer.\n' +
-      '* Variabler och programtillstånd, vägval, iteration.\n' +
-      '* Aritmetiska och logiska uttryck, strängar och textbehandling.\n' +
-      '* Sammansatta datatyper, parametrisering och funktionsabstraktion.\n' +
-      '* Filbegreppet, standardbibliotek och felhantering.\n' +
-      '* Referenser kontra värden, dynamiska datastrukturer.\n' +
-      '* Introduktion till objektbegreppet.\n' +
-      '* Problemlösning, programstruktur och dokumentation.\n' +
-      '\n' +
-      '## Litteratur\n' +
-      'Allen Downey, Jeffrey Elkner, Chris Meyers.\n' +
-      '\n' +
-      '*How to Think Like a Computer Scientist: Learning with Python*,\n' +
-      '\n' +
-      'Green Tea Press, 2002.\n' +
-      '\n' +
-      'ISBN: 0971677506\n' +
-      '\n' +
-      'Kurslitteraturen finns även anpassad till Java och C++\n' +
-      '\n' +
-      '(Alternativ kurslitteratur: *Alan Gauld, Learning to program*)', rewards1);
-    lbEntry1 = {name: 'anonymous', score: 200};
-    lbEntry2 = {name: 'anonymous', score: 190};
-    lbEntry3 = {name: 'you', score: 180};
-    lbEntry4 = {name: 'anonymous', score: 178};
-    lbEntry5 = {name: 'anonymous', score: 165};
-    leaderboard = [lbEntry1, lbEntry2, lbEntry3, lbEntry4, lbEntry5];
-    const rewards2 = newRewards(90, 180, ['bronze_medal_badge', 'silver_trophy_badge'], leaderboard);
-    const course1 = newCourse('1', 'Course name 2', 'D0010E', 'Course info', rewards2);
-    const rewards3 = newRewards(40, 50, false, false);
-    const course2 = newCourse('2', 'Course name 3', 'D0011E', 'Course info', rewards3);
-    const rewards4 = newRewards(false, false, ['gold_medal_badge', 'gold_trophy_badge', 'computer_badge',
-      'bronze_medal_badge', 'silver_trophy_badge', 'bronze_trophy_badge'], false);
-    const course3 = newCourse('3', 'Course name 4', 'D0012E', 'Course info', rewards4);
-    this.courses[0] = course0;
-    this.courses[1] = course1;
-    this.courses[2] = course2;
-    this.courses[3] = course3;*/
   }
   CreateCourse(id, name, code, course_info, progress, score, badges, leaderboard) {
     const progValue = progress ? 0 : false;
