b4654dc706705597f92af2ef6a94a78ea47f3863
Linn Danielsson <danielsson.linn@gmail.com>
2017-12-01 10:10:46 +0100

    Added some more user info

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -4,14 +4,22 @@ import { BackendService } from './backend.service';
 @Injectable()
 export class UserService {
   userInfo =  {
+    id: '',
     userName: '',
-    id: ''
+    admin: false,
+    access: '', // basic is student, advanced is teacher or assistant
   };
   updated = false;
   constructor(private backendService: BackendService) { }
 
   getMe() {
-    this.backendService.getMe().then(data => {this.userInfo.id = data['_id']; this.userInfo.userName = data['username']; });
+    this.backendService.getMe().then(data => {
+      console.log('User data:', data);
+      this.userInfo.id = data['_id'];
+      this.userInfo.userName = data['username'];
+      this.userInfo.admin = data['admin'];
+      this.userInfo.access = data['access'];
+    });
   }
 
 }
