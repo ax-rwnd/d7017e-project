ed21f7a4a475a98ae4201f6ee17993091695997d
nilfit <nils.fitinghoff@gmail.com>
2017-12-01 15:39:23 +0100

    add route to delete an assignment

--- Backend/routes/api/courses.js
+++ Backend/routes/api/courses.js
@@ -811,6 +811,25 @@ module.exports = function(router) {
         }).catch(next);
     });
 
+    router.delete('/:course_id/assignments/:assignment_id', function (req, res, next) {
+        var course_id = req.params.course_id;
+        var assignment_id = req.params.assignment_id;
+        if (!mongoose.Types.ObjectId.isValid(course_id)) {
+            return next(errors.BAD_INPUT);
+        }
+        if (!mongoose.Types.ObjectId.isValid(assignment_id)) {
+            return next(errors.BAD_INPUT);
+        }
+        queries.checkIfTeacherOrAdmin(req.user.id, course_id, req.user.access)
+        // ensure the course actually owns the assignment
+        // TODO: use lib/perssion
+        .then(() => queries.deleteAssignment(assignment_id))
+        .then(() => {
+            // respond with empty body
+            res.json({});
+        }).catch(next);
+    });
+
     router.get('/:course_id/invitelink', function (req, res, next) {
         var course_id = req.params.course_id;
 
