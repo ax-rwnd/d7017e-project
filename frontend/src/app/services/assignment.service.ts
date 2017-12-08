4bce0424e23bc730856c6d23ea77dee15ab82761
Linn Danielsson <danielsson.linn@gmail.com>
2017-12-08 10:11:50 +0100

    Added comments

--- frontend/src/app/services/assignment.service.ts
+++ frontend/src/app/services/assignment.service.ts
@@ -8,21 +8,19 @@ import { Subject } from 'rxjs/Subject';
 @Injectable()
 export class AssignmentService {
   courseAssignments = {};
-  assignmentsSub = new Subject<any>();
-  groupSub = new Subject<any>();
+  assignmentsSub = new Subject<any>(); // used when change of assignments occur
+  groupSub = new Subject<any>(); // used when a change of groups, ex creating group, occur
 
   constructor(private backendService: BackendService) {
 
   }
 
   getAssignmentsForCourse(course_id): Promise<any> {
-    // Set assignments to a course
+    // Get assignments for a course from database, called from course service
 
     return new Promise((resolve, reject) => {
-      this.backendService.getCourseAssignments(course_id)
+      this.backendService.getCourseAssignments(course_id) // Retrieve info from database
         .then(assignmentsResponse => {
-          console.log('AssignmentsResp:', assignmentsResponse);
-          // this.AddCourseAssignments(course_id, assignmentsResponse['assignments']);
           this.setAssignmentsForCourse(course_id, assignmentsResponse['assignments']);
           resolve();
         })
@@ -31,9 +29,13 @@ export class AssignmentService {
   }
 
   getAssignmentGroups(course_id, backendService) {
-    console.log('getting groups');
+    /* Get groups with assignments for a course from the database.
+       From the database, will get a list of groups which is set for
+       the course in function 'setGroupsForCourse'.
+    */
+
     return new Promise((resolve, reject) => {
-      backendService.getAssignmentGroupsCourse(course_id)
+      backendService.getAssignmentGroupsCourse(course_id) // Retrieve info from database
         .then(response => {
           const groups = response['assignmentgroups'];
           this.setGroupsForCourse(course_id, groups);
@@ -44,17 +46,21 @@ export class AssignmentService {
   }
 
   setGroupsForCourse(course_id: string, groups: any[]) {
+    // Sets a list of Assignmentsgroup, groupList, to a course.
+
     const groupList = getGroupList(groups);
-    console.log('GroupList:', groupList);
     if (this.courseAssignments[course_id]) {
+      // Assignments has already been 'created', only need to set groups
       this.courseAssignments[course_id]['groups'] = groupList;
-    } else { // Not defined yet
+    } else {
+      // Assignments for course hasn't been set yet, set new Assignments object
       this.courseAssignments[course_id] = newAssignments(groupList, '');
     }
-    console.log('courseAssignments:', this.courseAssignments);
   }
 
   setAssignmentsForCourse(course_id: string, assignments: any[]) {
+    // Works similar as setGroupsForCourse but with assignments for course instead
+
     const assignList = getAssignmentList(assignments);
     if (this.courseAssignments[course_id]) {
       this.courseAssignments[course_id]['assignments'] = assignList;
@@ -64,18 +70,25 @@ export class AssignmentService {
   }
 
   addAssignment(assign: Object, course_id: string) {
+    // Used when creating assignment, adds new assignment to course and push the
+    // new value to components that has subscribed to assignmentSub
+
     const assignment = newAssignment(assign['_id'], assign['name'], assign['languages'], assign['description']);
     this.courseAssignments[course_id]['assignments'].push(assignment);
     return this.assignmentsSub.next(this.courseAssignments[course_id]['assignments']);
   }
 
   addAssignmentGroup(gr: Object, course_id: string) {
+    // Works similar to addAssignment function with subscription etc
+
     const group = newAssignmentsgroup(gr['_id'], gr['name'], []);
     this.courseAssignments[course_id]['groups'].push(group);
     return this.groupSub.next(this.courseAssignments[course_id]['groups']);
   }
 
   removeAssignmentGroup(gr: Object, course_id: string) {
+    // Works similar to addAssignmentGroup but instead of add new group a group is deleted
+
     const index = this.courseAssignments[course_id]['groups'].indexOf(gr);
     if (index !== -1) {
       this.courseAssignments[course_id]['groups'].splice(index, 1);
@@ -83,24 +96,33 @@ export class AssignmentService {
     }
   }
 
-  GetAssignment(course_id: string, group_id: string, assignment_id: string): Assignment { // when you click on an assignment
+  GetAssignment(course_id: string, group_id: string, assignment_id: string): Assignment {
+    // Called whn a user (student) clicks on an assignment, search through groups and
+    // assignments and then return assignment to be displayed
+
     const groups = this.courseAssignments[course_id].groups;
-    const group = groups.find((current) => current.id === group_id);
+    const group = groups.find((current) => current.id === group_id); // find group matching group_id
     const assignments = group.assignments;
-    return assignments.find((current) => current.id === assignment_id);
+    return assignments.find((current) => current.id === assignment_id); // return assignment matching id in group
   }
 }
 
-function getGroupList(groups: any): Assignmentsgroup[] {
+function getGroupList(groups: any[]): Assignmentsgroup[] {
+  // Goes through array of groups and returns an Assignmentsgroup, sets
+  // id, name and a list of Assignment for each group
+
   const groupList: Assignmentsgroup[] = [];
   for (const group of groups) {
-    const assignList = getAssignmentList(group.assignments);
+    const assignList = getAssignmentList(group.assignments); // array of assignments
     groupList.push(newAssignmentsgroup(group._id, group.name, assignList));
   }
   return groupList;
 }
 
-function getAssignmentList(assignments: any): Assignment[] {
+function getAssignmentList(assignments: any[]): Assignment[] {
+  // Works similar to getGroupList but with Assignment instead, an Assignment has
+  // id, name, languages and description.
+
   const assignmentsList: Assignment[] = [];
   for (const assign of assignments) {
     let assignment = assign;
